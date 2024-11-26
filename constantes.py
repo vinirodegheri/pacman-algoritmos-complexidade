dc0d6acc3342f45d61aa9365a889c493a8a788aa
Vinícius <vinicius7ti@gmail.com>
2024-11-25 21:20:37 -0300

    First commit

---
+++ constantes.py
@@ -0,0 +1,31 @@
+import os
+
+DIRETORIO = os.path.dirname(__file__)
+DIRETORIO_IMAGENS = os.path.join(DIRETORIO, 'imagens')
+DIRETORIO_SONS = os.path.join(DIRETORIO, 'sons')
+FONTE_SIZE =   36
+
+
+FPS = 60
+CONST_DIFICULDADE = 3
+
+COMIDA_TUPLE =  (99/2, 99/2)
+PACMAN_TUPLE = (32, 31)
+PACMAN_ESCALE = 1.2
+PACMAN_ESPESSURA = 60
+ESPESSURA = 10*2.5
+LARGURA =720
+ALTURA = (13*PACMAN_ESPESSURA)+(ESPESSURA*2)
+
+AZUL = (0,0,50) 
+ROXO =  (164,76,163) 
+BRANCO = (255,255,255)
+PRETO = (0,0,0)
+
+CREDITOS = [
+"CREDITOS FINAIS",
+" ",
+"Vinicius - Cordenador de Atividades, Desenvovimento do jogo, Desenvovimento do ipynb ", 
+
+
+]
